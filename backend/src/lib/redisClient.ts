fbeb515251c5246a3d7d62a71773438c20ae686c
Your Name <you@example.com>
2018-09-01 13:56:10 +0900

    pinter/redis client created

---
+++ backend/src/lib/redisClient.ts
@@ -0,0 +1,88 @@
+import * as redis from 'redis';
+
+class RedisClient {
+    public client: redis.RedisClient | null = null;
+    public connectedTime: number | null = null;
+
+    get connected() {
+        if (!this.client) return false;
+        return this.client.connected;
+    }
+
+    public connect() {
+        const p = new Promise(
+            (resolve, reject) => {
+                const client = redis.createClient({
+                    // host,
+                    // password
+                });
+
+                client.on('error', (err) => {
+                    console.log('Redis Error: ', err);
+                    reject(err);
+                });
+
+                client.on('ready', () => {
+                    this.connectedTime = Date.now();
+                    console.log('Redis is ready');
+                    resolve();
+                });
+                this.client = client;
+            },
+        );
+        return p;
+    }
+
+    public async remove(key: string) {
+        if (!this.connected || !this.client) {
+            await this.connect();
+        }
+
+        if (!this.client) return;
+        this.client.del(key);
+        console.log('removing key %s', key);
+    }
+
+    public async setCache(key: string, value: string, duration: number) {
+        if (!this.connected) {
+            await this.connect();
+        }
+
+        const p = new Promise((resolve, reject) => {
+            if (!this.connected || !this.client) {
+                reject(new Error('redis not connected'));
+                return;
+            }
+
+            this.client.set(key, value, 'EX', duration, (err, reply) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(reply); 
+            });
+        });
+
+        return p;
+    }
+
+    public async getCache(key: string) {
+        if (!this.connected) {
+          await this.connect();
+        }
+        const p = new Promise((resolve, reject) => {
+          this.client.get(key, (err, reply) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+            resolve(reply);
+          });
+        });
+        return p;
+    }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
