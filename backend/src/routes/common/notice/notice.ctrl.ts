3fec7df6ee41ca4a1b385b7bc1bb44d282005a48
Your Name <you@example.com>
2018-09-16 11:20:12 +0900

    pinter/ Send message Write API and send data to Socket

--- backend/src/routes/common/notice/notice.ctrl.ts
+++ backend/src/routes/common/notice/notice.ctrl.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import Notice, { INotice } from '../../../database/models/Notice';
 import NoticeMessage, { INoticeMessage } from '../../../database/models/NoticeMessage';
 import Follow, { IFollow } from '../../../database/models/Follow';
-import { serializeNoticeRoom, serializeNoticeMessage } from '../../../lib/serialize';
+import { serializeNoticeRoom } from '../../../lib/serialize';
 import socketServer from '../../../lib/socket';
 
 export const getNoticeRoom = async (req: Request, res: Response): Promise<any> => {
@@ -42,44 +42,43 @@ export const getNoticeRoom = async (req: Request, res: Response): Promise<any> =
 
 export const sendMessage = async (req: Request, res: Response): Promise<any> => {
     type BodySchema = {
-        senderId: string,
-        recipientId: string,
-        noticeId: string,
         message: string,
     }
 
-    const { senderId, noticeId, message, recipientId }: BodySchema = req.body;
+    const { message }: BodySchema = req.body;
     const userId: string = req['user']._id;
+    const username: string = req['user'].displayName;
 
     try {
-        const followUsers: IFollow[] = await Follow.find({ follower: userId }).lean();
-        const userIds = followUsers.map(users => users.following);
-        const notices: INotice[] = await Promise.all(userIds.map(userId => Notice.findOne({ creator: userId }).lean()));
+        const followingUsers: IFollow[] = await Follow.find({ follower: userId }).lean();
 
-        const noticeMessage: INoticeMessage = await new NoticeMessage({
-            sender: senderId,
-            recipient: recipientId,
-            notice: noticeId,
-            message,
-        }).save();
+        const followingIds = followingUsers.map(user => user.following);
 
-        const messageData = await NoticeMessage.findById(noticeMessage._id)
+        const following: INotice[] =  await Promise.all(followingIds.map(follower => Notice.findOne({ creator: follower })
         .populate({
-            path: 'sender',
+            path: 'creator',
             select: 'username profile.displayName profile.thumbnail'
         })
-        .populate({
-            path: 'recipient',
-            select: 'username profile.displayName profile.thumbnail'
-        })
-        .lean();
+        .lean()));
+        
+        const messageData: INoticeMessage[] = await Promise.all(following.map(user => {
+            const notice = new NoticeMessage({
+                sender: userId,
+                recipient: user.creator._id,
+                notice: user._id,
+                message: `${user.creator.profile.displayName}님 ${username}님이 ${message} 하였습니다`,
+            }).save();
+            return notice;
+        })) 
 
-        notices.map(notice => socketServer.getSocket.to(notice._id).emit('new-message', {
-            messageWithData: serializeNoticeMessage(messageData),
-        }))
+        messageData.map(message => {
+            socketServer.getSocket.to((message.notice as any)).emit('new-message', {
+                message: message.message,
+            });
+        });
 
         res.json({
-            messageWithData: serializeNoticeMessage(messageData),
+            messageWithData: messageData,
         });
     } catch (e) {
         res.status(500).json(e);
